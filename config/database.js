b7a5ee66ecd4ca2ba845024ff53c827240477247
89885512495 <v.dunaev@yahoo.com>
2021-05-20 07:39:42 +0300

    auto add profile and user from chainsaw

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const parse = require('pg-connection-string').parse;
 
 module.exports = ({ env }) => {
+  // console.log('------------env config--------------');
 
-  if(env('NODE_ENV') === 'production'){
+  // if(env('NODE_ENV') === 'production'){
     const config = parse(process.env.DATABASE_URL);
+    const config_chainsaw = parse(process.env.DATABASE_URL_CHAINSAW);
     return {
       defaultConnection: 'default',
       connections: {
@@ -25,23 +27,41 @@ module.exports = ({ env }) => {
             rejectUnauthorized: false
           },
         },
+        chainsaw: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config_chainsaw.host,
+            port: config_chainsaw.port,
+            database: config_chainsaw.database,
+            username: config_chainsaw.user,
+            password: config_chainsaw.password,
+            ssl: {
+              rejectUnauthorized: false
+            }
+          },
+          options: {
+            ssl: true,
+            rejectUnauthorized: false
+          },
+        },
       },
     }
-  }
+  // }
   
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
-      },
-    },
-  }
+  // return {
+  //   defaultConnection: 'default',
+  //   connections: {
+  //     default: {
+  //       connector: 'bookshelf',
+  //       settings: {
+  //         client: 'sqlite',
+  //         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  //       },
+  //       options: {
+  //         useNullAsDefault: true,
+  //       },
+  //     },
+  //   },
+  // }
 };
